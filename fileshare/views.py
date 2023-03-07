87d62270e05a9f9e4659abf7fe6de36e52e61463
Vallabh <tiwarivallabh27@gmail.com>
2023-03-07 10:50:14 +0530

    added views for upload and upload-success

--- fileshare/views.py
+++ fileshare/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import Http404, render, redirect
+from django.conf import settings
+from django.contrib.sites.models import Site
 
-# Create your views here.
+from .models import File, Folder
+from shorturl.models import ShortUrl
+from .forms import FileForm
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+def home(request):
+    return render(request, "fileshare/home.html")
+
+
+@api_view(["GET", "POST"])
+def upload(request):
+    form = FileForm()
+    if request.method == "POST":
+        form = FileForm(request.POST, request.FILES)
+        files = request.FILES.getlist("file")
+
+        if form.is_valid:
+            folder = Folder.objects.create()
+            for f in files:
+                File.objects.create(file=f, folder=folder)
+
+            if len(files) > 1:
+                folder.zip_files()
+            folder.save()
+
+            return Response({"folder_id": folder.name})
+
+    context = {"form": form}
+    return render(request, "fileshare/upload.html", context)
+
+
+@api_view(["GET"])
+def upload_success(request, id):
+    try:
+        folder = Folder.objects.get(name=id)
+    except:
+        raise Http404
+
+    current_site = Site.objects.get_current()
+    url = f"{current_site.domain}" + settings.MEDIA_URL
+
+    if folder.zipped:
+        url += f"{folder.name}" + ".zip"
+    else:
+        url += f"{folder.file_set.first().file}"
+
+    short_url = ShortUrl.objects.create(link=url)
+    short_url = f"{current_site.domain}/" + short_url.id
+
+    folder.delete()
+    context = {"url": short_url}
+
+    return Response(context)
