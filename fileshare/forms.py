202744755790e9a0a1a90c140c376400bd139962
Vallabh <tiwarivallabh27@gmail.com>
2023-03-07 10:54:00 +0530

    model form for files

---
+++ fileshare/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import File
+
+
+class FileForm(forms.ModelForm):
+    class Meta:
+        model = File
+        fields = ["file"]
+        widgets = {
+            "file": forms.ClearableFileInput(attrs={"multiple": True}),
+        }
