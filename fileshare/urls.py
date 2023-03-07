8188955cd5a44955bb7e2162cb076ffa7f111961
Vallabh <tiwarivallabh27@gmail.com>
2023-03-07 10:52:19 +0530

    added urls for upload and upload-success

---
+++ fileshare/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import home, upload, upload_success
+
+app_name = "fileshare"
+urlpatterns = [
+    path("", home, name="home"),
+    path("upload/", upload, name="upload"),
+    path("upload-success/<slug:id>/", upload_success, name="upload-success"),
+]
