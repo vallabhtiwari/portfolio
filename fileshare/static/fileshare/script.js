1e11fbcc5027bf1466c8e318352b40ac4d2f3b96
Vallabh <tiwarivallabh27@gmail.com>
2023-03-08 11:59:29 +0530

    making requests through axios, to add progress bar

--- fileshare/static/fileshare/script.js
+++ fileshare/static/fileshare/script.js
@@ -4,29 +4,46 @@ fileForm.addEventListener("submit", function(event) {
   event.preventDefault()
   var formdata = new FormData(fileForm)
   fileForm.reset()
-  var url = `${window.location.href}`
-  fetch(url, {
-    method: "POST",
-    body: formdata,
-  })
-    .then((response) => response.json())
-    .then((data) => {
+  var url = "/share/upload/"
 
-      url = "/share/upload-success/" + data["folder_id"]
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data"
+    },
+    onUploadProgress: function(progress) {
+      var percent = (progress.loaded / progress.total) * 100
+      percent = percent.toFixed(2)
+      var progressBarContainer = document.getElementById("progress-bar-container")
+      var progressBar = document.getElementById("progress-bar")
+      progressBarContainer.classList.remove("d-none")
+      progressBarContainer.setAttribute("aria-valuenow", percent)
+      progressBar.style.width = `${percent}%`
+    }
+  }
 
-      fetch(url, {
-        method: "GET",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          modalLink.innerText = data["url"]
+  axios.post(url, formdata, config)
+    .then(function(response) {
+      url = "/share/upload-success/" + response.data["folder_id"]
+      axios.get(url)
+        .then(function(response) {
+          modalLink.innerText = response.data["url"]
           document.getElementById("modal-button").click()
         })
+        .catch(function(error) {
+          console.log(error)
+        })
     })
+    .catch(function(error) {
+      //handle error
+      console.log(error);
+    });
 })
 
 document.getElementById("clip").addEventListener("click", function() {
   navigator.clipboard.writeText(modalLink.innerText)
   document.getElementById("clip-icon").className = "bi bi-clipboard-check"
 })
-
+document.getElementById("modal-close-button").addEventListener("click", function() {
+  document.getElementById("clip-icon").className = "bi bi-clipboard"
+  document.getElementById("progress-bar-container").classList.add("d-none")
+})
