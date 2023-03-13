12048fdb9fe4c57db6a3d233853e12f9f73c52f8
Vallabh <tiwarivallabh27@gmail.com>
2023-03-13 18:30:49 +0530

    added background task to delete folders

--- fileshare/tasks.py
+++ fileshare/tasks.py
@@ -5,21 +5,39 @@ import shutil
 
 from shorturl.models import ShortUrl
 from datetime import datetime, timedelta
+from .models import Folder
 
 
 @shared_task(bind=True)
 def zip_files(self, name):
-    os.chdir(settings.MEDIA_ROOT)  # change the working directory to the folder
-    shutil.make_archive(base_name=f"{name}", format="zip", root_dir=f"{name}")
-    shutil.rmtree(f"{name}")  # removed the folder cause not needed
+    try:
+        os.chdir(settings.MEDIA_ROOT)  # change the working directory to the folder
+        shutil.make_archive(base_name=f"{name}", format="zip", root_dir=f"{name}")
+        shutil.rmtree(f"{name}")  # removed the folder cause not needed
+    except:
+        return False
 
-    return "Folder zipped"
+    return True
 
 
+# to delete a folder
+@shared_task(bind=True)
+def delete_folder(self, folder_name):
+    try:  # folder is not created if first file is invalid, so exception might occur
+        os.chdir(settings.MEDIA_ROOT)
+        shutil.rmtree(f"{folder_name}")
+    except:
+        pass
+
+    Folder.objects.filter(name=folder_name).delete()
+    return True
+
+
+# periodic task to delete fils older than 10 days
 @shared_task(bind=True)
 def delete_files(self):
     current = datetime.now()
-    delta = timedelta(days=10)
+    delta = timedelta(days=10)  # timedelta for 10days
     old = ShortUrl.objects.filter(date_created__lte=current - delta)
     os.chdir(settings.MEDIA_ROOT)  # change the working directory to the folder
     count = 0
@@ -36,4 +54,4 @@ def delete_files(self):
             pass
         o.delete()
 
-    return f"{count} Old files deleted"
+    return f"{count} Old folders deleted"
