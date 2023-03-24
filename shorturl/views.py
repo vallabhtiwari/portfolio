08c2aeba3be0e18d95aa7454af07bf10478b60fe
Vallabh <tiwarivallabh27@gmail.com>
2023-03-24 13:23:05 +0530

    added view to shorten urls

--- shorturl/views.py
+++ shorturl/views.py
@@ -18,3 +18,30 @@ def redirect_to_full_url(request, id):
         raise Http404
 
     return redirect(url.link)
+
+
+def home(request):
+    return render(request, "shorturl/home.html")
+
+
+@api_view(["GET", "POST"])
+def shorten_url(request):
+    try:
+        url = request.POST.get("url")
+        url_form_field = URLField()
+        url = url_form_field.clean(url)
+        short_url = ShortUrl.objects.create(link=url)
+        current_site = Site.objects.get_current()
+        short_url = f"{current_site.domain}/{short_url.id}"
+    except ValidationError:
+        return Response(
+            {"message": "Please enter a valid url!!"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    except:
+        return Response(
+            {"message": "Internal Server Error. Please try again..."},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+    return Response({"url": short_url}, status=status.HTTP_200_OK)
