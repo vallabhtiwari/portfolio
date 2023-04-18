b69bc6f91c36da4eee78a1331fef9be2a9ac3c8a
Vallabh <tiwarivallabh27@gmail.com>
2023-04-18 22:57:05 +0530

    date time field for feedback

--- base/models.py
+++ base/models.py
@@ -7,3 +7,7 @@ class Feedback(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     feedback = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
