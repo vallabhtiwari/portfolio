911de18b60b8ddc233d618fc87d49927d95100ee
Vallabh <tiwarivallabh27@gmail.com>
2023-04-20 09:37:26 +0530

    added periodic task to delete feedbacks older than 10 days

--- base/tasks.py
+++ base/tasks.py
@@ -2,7 +2,10 @@ from celery import shared_task
 from django.core.mail import send_mail
 from portfolio import settings
 
+from .models import Feedback
+from datetime import datetime, timedelta
 
+# background task to send mails
 @shared_task(bind=True)
 def send_email(self, email_id):
     mail_subject = f"Hi {email_id.split('@')[0]}"
@@ -20,3 +23,11 @@ def send_email(self, email_id):
         recipient_list=[to_email],
         fail_silently=True,
     )
+
+
+# background task to delete feedbacks older than 10 days
+@shared_task(bind=True)
+def delete_feedbacks(self):
+    current = datetime.now()
+    delta = timedelta(days=10)  # timedelta for 10days
+    Feedback.objects.filter(date_created__lte=current - delta).delete()
--- portfolio/celery.py
+++ portfolio/celery.py
@@ -16,7 +16,11 @@ app.conf.beat_schedule = {
     "delete_files_older_than_10_days": {
         "task": "fileshare.tasks.delete_files",
         "schedule": crontab(minute=0, hour=0),
-    }
+    },
+    "delete_feedbacks_older_than_10_days": {
+        "task": "base.tasks.delete_feedbacks",
+        "schedule": crontab(minute=0, hour=1),
+    },
 }
 #################
 app.autodiscover_tasks()
